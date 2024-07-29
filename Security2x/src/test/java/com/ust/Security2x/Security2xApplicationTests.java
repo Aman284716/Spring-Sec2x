172ef13698796b38f79b17bb85e3d37f8d67b723
Aman284716 <284716@ust.com>
2024-07-29 15:41:05 +0530

    Docker added final

--- Security2x/src/test/java/com/ust/Security2x/Security2xApplicationTests.java
+++ Security2x/src/test/java/com/ust/Security2x/Security2xApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Security2xApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
